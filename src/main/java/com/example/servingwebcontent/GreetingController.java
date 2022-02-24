530f5ed8fcf1015bdb25c8c92ea6791b3ec433be
wuzhongjian <wuzhongjian@cmss.chinamobile.com>
2022-02-24 15:33:49 +0800

    健康检查
    Code Source From: Self Code
    Description:  健康检查
    Jira:
    市场项目编号（名称）：【Optional】

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -3,10 +3,7 @@ package com.example.servingwebcontent;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class GreetingController {
@@ -20,6 +17,7 @@ public class GreetingController {
 	@ResponseStatus(HttpStatus.OK)
 	@CrossOrigin
 	@GetMapping(value = "/health")
+	@ResponseBody
 	public Boolean checkHealth() {
 		return true;
 	}
